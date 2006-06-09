8fb8cf60d4289253ccc8811901855217ae884dc8
Andrew Wright <andrew@jivesoftware.com>
2006-06-09 16:03:56 +0000

    component stuff

--- server/src/java/org/jivesoftware/phone/PhonePlugin.java
+++ server/src/java/org/jivesoftware/phone/PhonePlugin.java
@@ -8,10 +8,6 @@
  */
 package org.jivesoftware.phone;
 
-import java.io.File;
-import java.sql.Connection;
-import java.util.Collection;
-
 import org.jivesoftware.database.DbConnectionManager;
 import org.jivesoftware.phone.database.DatabaseUtil;
 import org.jivesoftware.phone.util.PhoneConstants;
@@ -28,42 +24,44 @@ import org.jivesoftware.wildfire.interceptor.InterceptorManager;
 import org.xmpp.component.Component;
 import org.xmpp.component.ComponentException;
 import org.xmpp.component.ComponentManager;
-import org.xmpp.component.ComponentManagerFactory;
 import org.xmpp.packet.IQ;
 import org.xmpp.packet.JID;
-import org.xmpp.packet.Message;
 import org.xmpp.packet.Packet;
 import org.xmpp.packet.Presence;
 
-public abstract class PhonePlugin  implements Plugin, Component, PhoneConstants {
-	
-	// FIXME: a plugin should get the server reference throuh initialization ;jw
-	protected XMPPServer server = XMPPServer.getInstance();
-	protected PhoneManager phoneManager;
-	protected PacketHandler packetHandler;
-	protected PresenceLayerer interceptor;
-	private boolean isComponentReady;
-	private JID componentJID;
-	private ComponentManager componentManager = ComponentManagerFactory.getComponentManager();
-	
+import java.io.File;
+import java.sql.Connection;
+import java.util.Collection;
+
+public abstract class PhonePlugin implements Plugin, Component, PhoneConstants {
+
+    // FIXME: a plugin should get the server reference throuh initialization ;jw
+    protected XMPPServer server = XMPPServer.getInstance();
+    protected PhoneManager phoneManager;
+    protected PacketHandler packetHandler;
+    protected PresenceLayerer interceptor;
+    private boolean isComponentReady;
+    private JID componentJID;
+    private ComponentManager componentManager;
+
     /**
      * Send a packet to all user sessions
      */
     public void sendPacket2User(String user, Packet message) {
-		message.setFrom(getComponentJID());
+        message.setFrom(getComponentJID());
         SessionManager sessionManager = server.getSessionManager();
         Collection<ClientSession> sessions = sessionManager.getSessions(user);
-        if (sessions.size()==0) {
-        	return;
+        if (sessions.size() == 0) {
+            return;
         }
         for (ClientSession session : sessions) {
-        	message.setTo(session.getAddress());
+            message.setTo(session.getAddress());
             sendPacket(message);
         }
     }
-    
+
     public abstract void initPhoneManager();
-    
+
     public void initializePlugin(PluginManager manager, File pluginDirectory) {
         init();
     }
@@ -100,10 +98,12 @@ public abstract class PhonePlugin  implements Plugin, Component, PhoneConstants 
                 Log.info("Registering phone plugin as a component");
                 componentManager.addComponent(getName(), this);
             }
-            catch (ComponentException e) {
+            catch (Throwable e) {
                 Log.error(e.getMessage(), e);
                 // Do nothing. Should never happen.
-                componentManager.getLog().error(e);
+                if (componentManager != null) {
+                    componentManager.getLog().error(e);
+                }
             }
         }
 
@@ -135,7 +135,7 @@ public abstract class PhonePlugin  implements Plugin, Component, PhoneConstants 
             // Do nothing. Should never happen.
             componentManager.getLog().error(e);
         }
-        
+
         interceptor.restoreCompletely();
 
         // If there isn't a manager instance established don't try to destroy it.
@@ -149,7 +149,7 @@ public abstract class PhonePlugin  implements Plugin, Component, PhoneConstants 
         // Remove OnPhonePacketInterceptor as a session event listener
         SessionEventDispatcher.removeListener(interceptor);
     }
-    
+
     /**
      * sets isComponentReady to true so we start accepting requests
      */
@@ -170,24 +170,23 @@ public abstract class PhonePlugin  implements Plugin, Component, PhoneConstants 
     }
 
     /**
-     * Restart the plugin, used by the web configuration after the 
+     * Restart the plugin, used by the web configuration after the
      * configuration changed
      */
     public void restart() {
-    	destroy();
-    	try {
-        	destroy();
-    		Thread.sleep(1 * JiveConstants.SECOND);
-    		init();
-    		Thread.sleep(1 * JiveConstants.SECOND);
-    	}
-    	catch (InterruptedException e) {
-    		Log.error(e);
-    	}
+        try {
+            destroy();
+            Thread.sleep(1 * JiveConstants.SECOND);
+            init();
+            Thread.sleep(1 * JiveConstants.SECOND);
+        }
+        catch (InterruptedException e) {
+            Log.error(e);
+        }
     }
-    
+
     public abstract PhoneManager getPhoneManager();
-    
+
     /**
      * Processes all IQ packets passed in, other types of packets will be ignored
      *
@@ -195,50 +194,24 @@ public abstract class PhonePlugin  implements Plugin, Component, PhoneConstants 
      */
     public void processPacket(Packet packet) {
 
-    	// race, packatHandler not yet there
-    	if (packetHandler==null) {
-    		return;
-    	}
-    	
+        // race, packatHandler not yet there
+        if (packetHandler == null) {
+            return;
+        }
+
         if (!isComponentReady) {
             Log.warn("Phone component not ready, ignoring request");
             return;
         }
 
         if (!(packet instanceof IQ)) {
-        	if (packet instanceof Message) {
-        		processMessage((Message) packet);
-        		return;
-        	}
-            Log.debug("Phone components expects packets to be IQ packets, ignoring!");
             return;
         }
 
         IQ iq = (IQ) packet;
+        // This is where actions are processed, dialing, etc.
         packetHandler.processPacket(iq);
 
-    }
-    
-    void processMessage(Message msg) {
-    	// FIXME: check whether the message sender is really a subscribed phone user
-    	String bdy = msg.getBody();
-    	if (bdy!=null) {
-    		if (bdy.startsWith("call")) {
-    			int idx = bdy.indexOf(' ');
-    			if (idx>0) {
-    				String num = bdy.substring(idx+1);
-    				Message ret = new Message();
-    				try {
-						phoneManager.originate(msg.getFrom().getNode(), num);
-						ret.setBody("Dialing "+num+"...");
-					} catch (PhoneException e) {
-						ret.setBody("Got exception "+e);
-						Log.error(e);
-					}
-    				sendPacket2User(msg.getFrom().getNode(), ret);
-    			}
-    		}
-    	}
     }
 
     /**
@@ -250,9 +223,9 @@ public abstract class PhonePlugin  implements Plugin, Component, PhoneConstants 
      */
     public void initialize(JID jid, ComponentManager componentManager) throws ComponentException {
         this.componentJID = jid;
-        // this.componentManager = componentManager;
+        this.componentManager = componentManager;
     }
-    
+
 
     /**
      * Used to send a packet with this component
@@ -277,14 +250,14 @@ public abstract class PhonePlugin  implements Plugin, Component, PhoneConstants 
         return componentJID;
     }
 
-	public void restorePresence(String user) {
-		interceptor.restorePresence(user);
-	}
+    public void restorePresence(String user) {
+        interceptor.restorePresence(user);
+    }
 
-	public void setPresence(String user, Presence p) {
-		interceptor.setPresence(user, p);
-	}
-	
-	public abstract PhoneOption[] getOptions();
-       
+    public void setPresence(String user, Presence p) {
+        interceptor.setPresence(user, p);
+    }
+
+    public abstract PhoneOption[] getOptions();
+
 }
